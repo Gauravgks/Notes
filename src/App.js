a1b8954f7f68231a04275a8f9472b3cbc90802d4
Gaurav Singh <gaurav_singh@persistent.com>
2021-05-07 01:38:46 +0530

    added react-bootstrap code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 //import logo from './logo.svg';
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Display from "./components/Display";
 import Header from "./components/Header";
 import TextArea from "./components/TextArea";
 import Footer from "./components/Footer";
 
 function App() {
-
   // * Delete Notes from from Display area
-  const onDelete = (note)=>{
-    setNotes(notes.filter((e)=>{
-        return e!==note;
-    }))
-  }
-  // * Adding notes inserted from texterea into usestate 
-  const addNote = (title, desc)=>{
+  const onDelete = (note) => {
+    setNotes(
+      notes.filter((e) => {
+        return e !== note;
+      })
+    );
+  };
+  // * Adding notes inserted from texterea into usestate
+  const addNote = (title, desc) => {
     const myNote = {
-      title:title,
-      desc:desc,
-    }
+      title: title,
+      desc: desc,
+    };
     setNotes([...notes, myNote]);
-  }
+  };
   // * usestate for transfering notes to displayarea
-  const[notes, setNotes] =useState([ 
+  const [notes, setNotes] = useState([
     // {
     // title: "shopping",
     // desc:"Buy cloths"
@@ -32,17 +33,15 @@ function App() {
     // desc: "Read some books",
     // }
   ]);
-  
+
   return (
     <>
       <Header title="My Notes" />
-      <TextArea addNote = {addNote}/>
-      <Display notes = {notes} onDelete={onDelete}/>
+      <TextArea addNote={addNote} />
+      <Display notes={notes} onDelete={onDelete} />
       <Footer />
     </>
   );
 }
 
 export default App;
-
-
--- src/components/DisplayItem.js
+++ src/components/DisplayItem.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "../App.css";
+import { Button } from 'react-bootstrap';
+import {Accordion} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
 
 export const DisplayItem = ({ note, onDelete }) => {
   return (
-    <div className = "displayitem">
-      <div className="alert alert-success" role="alert">
-      <h4>{note.title}</h4>
-      <h4>{note.desc}</h4> 
-      <button className="btn btn-outline-danger" onClick={() => {onDelete(note)}}>Delete</button>
-      </div>
-    </div>
+    <Accordion>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button}  eventKey="0">
+          <h6>{note.title}</h6>
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>{note.desc}<br></br>
+          <br></br>
+          <button className="btn btn-outline-danger btn-sm" onClick={() => {onDelete(note)}}>Delete</button>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   );
 };
 
